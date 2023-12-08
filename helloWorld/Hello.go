ec650d834fd871a7491238672e2a3ff7bd086c00
waper97 <563667426@qq.com>
2023-12-09 00:54:00 +0800

    练习go语言的基础用法

--- helloWorld/Hello.go
+++ helloWorld/Hello.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	// 注释
+	/**
+	多行注释
+	*/
+	var today = "星期五"
+	var number = 6
+	var str string = "hello"
+	var num = 123
+	myString := "very good"
+	myNumber := 123
+	var num1 int
+	var str1 string
+	var flag bool
+	const name string = "fuck"
+
 	fmt.Println("hello world")
+	fmt.Println("自动换行")
+	fmt.Println("hello world" + "自动换行")
+	//格式化字符串  %s字符型 %d整数类型
+	fmt.Printf("今天是%s,在一周中排第%d", today, number)
+	println(str, num)
+	println(myString, myNumber)
+	println(num1)
+	fmt.Println("num1默认值为:", num1)
+	fmt.Println("str1默认值为:", str1)
+	fmt.Println("bool默认值为:", &flag)
+	fmt.Println("name:", name)
+
 }
